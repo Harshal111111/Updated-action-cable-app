7e16a3431458105b4ba5c91ade7fdb19b9423e23
Harshal <bordiaharshal@gmail.com>
2024-02-15 13:30:48 +0530

    updated model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   validates_uniqueness_of :username
 
   def self.generate
-    adjectives = ['Ancient', 'Creative', 'Dangerous', 'Effective', 'Flying', 'Gilded']
-    nouns = ['Highway', 'Intern', 'Jackhammer', 'Lion', 'Master']
+    adjectives = ['Test1', 'Test2', 'Test3', 'Test14']
+    nouns = ['User']
     number = rand.to_s[2..4]
     username = "#{adjectives.sample}-#{nouns.sample}-#{number}"
     create(username: username)
